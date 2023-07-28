3be35aea16c078e57f0998770d5105761d1cf0d8
Dixin <dixinyan@live.com>
2023-07-28 13:57:23 -0700

    Remove filter/no restriction.

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,22 +1,22 @@
 import threading
 import numpy
-import opennsfw2
+# import opennsfw2
 from PIL import Image
 from keras import Model
 
 from roop.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK = threading.Lock()
-MAX_PROBABILITY = 0.85
+# THREAD_LOCK = threading.Lock()
+# MAX_PROBABILITY = 0.85
 
 
 def get_predictor() -> Model:
     global PREDICTOR
 
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    # with THREAD_LOCK:
+    #     if PREDICTOR is None:
+    #         PREDICTOR = opennsfw2.make_open_nsfw_model()
     return PREDICTOR
 
 
@@ -27,17 +27,20 @@ def clear_predictor() -> None:
 
 
 def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
+    # image = Image.fromarray(target_frame)
+    # image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
+    # views = numpy.expand_dims(image, axis=0)
+    # _, probability = get_predictor().predict(views)[0]
+    # return probability > MAX_PROBABILITY
+    return False
 
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    # return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
+    return False
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    # _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
+    # return any(probability > MAX_PROBABILITY for probability in probabilities)
+    return False
